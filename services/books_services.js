b3500ec52c824fb426c7c7f91bed5b501b35b2c7
voidmimi <biyibioluwatosin@gmail.com>
2026-07-05 23:57:53 +0100

    Updated Id autogeneration to prevent Delete error

--- services/books_services.js
+++ services/books_services.js
@@ -4,22 +4,34 @@
 
 import { readBookInfo, writeBookInfo } from "../utils/fileHandler.js";
 export class BookService {
-   async addBook(bookInfo) {
-        // read  boookinfo from books.json through fileHandler.js
-   const books = await readBookInfo();
-        // make a new book object with properties     
-    const createdBook = {
-     id: String(Date.now()), // Generate a unique ID based on the current timestamp
-     title: bookInfo.title,
-     author: bookInfo.author,
-     available: true, // New books are available by default
-     createdAt: new Date().toISOString(), // Store the creation date in ISO format
-     updatedAt: new Date().toISOString() // Store the last updated date in ISO format
-    };
-// Add the new book to the books array and write it back to the JSON file
-        books.push(createdBook);
-        await writeBookInfo(books);
-        return createdBook;
+  async addBook(bookInfo) {
+    // read  boookinfo from books.json through fileHandler.js
+    const books = await readBookInfo();
+    // get the last book to get its id
+    let lastId;
+    if (books.length === 0) {
+      lastId = 0;
+    } else {
+      lastId = books.at(-1).id;
+    }
+    //
+    // make a new book object with properties
 
-}
+    const createdBook = {
+      id: lastId + 1, // Generate a unique ID based on the current timestamp
+      title: bookInfo.title,
+      author: bookInfo.author,
+      available: true, // New books are available by default
+      createdAt: new Date().toISOString(), // Store the creation date in ISO format
+      updatedAt: new Date().toISOString(), // Store the last updated date in ISO format
+    };
+    // Add the new book to the books array and write it back to the JSON file
+    books.push(createdBook);
+    await writeBookInfo(books);
+    return createdBook;
+  }
+
+  async borrowBook(id) {
+    const books = await readBookInfo();
+  }
 }
