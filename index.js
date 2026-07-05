0789841ccd11ea45262d2dc0c1c4b17b0ca19331
voidmimi <biyibioluwatosin@gmail.com>
2026-07-05 18:56:37 +0100

    added ejs to package.json, task 2 completed. update

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 // Entry point of the backend
 // Starts the Express server and connects routes
 // Example: import express, set up app, use routes, listen on PORT
+
+import express from "express";
+import bookRouter from "./routes/books_routes.js";
+
+const libraryApp = express();
+const PORT = 3000;
+
+libraryApp.set("view engine", "ejs"); // task 2 to be able to render the index.ejs
+
+libraryApp.use(express.json()); // necessary middleware to parse JSON request bodies
+libraryApp.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Library Book Tracker API" });
+});
+libraryApp.use("/books", bookRouter); // connect the bookRouter to the /books path
+libraryApp.use((req, res) => {
+  res.status(404);
+  res.json({ message: "Route not found" });
+});
+// START THE SERVER
+libraryApp.listen(PORT, () => {
+  console.log(` Library Server is running on http://localhost:${PORT}`);
+});
--- services/books_services.js
+++ services/books_services.js
@@ -1,3 +1,38 @@
 // Contains business logic for managing books
 // Example: addBook(), editBook(), deleteBook(), borrowBook(), returnBook()
 // Uses fileHandler (read/write JSON) to update books.json
+
+import { readBookInfo, writeBookInfo } from "../utils/fileHandler.js";
+export class BookService {
+  async addBook(bookInfo) {
+    // read  boookinfo from books.json through fileHandler.js
+    const books = await readBookInfo();
+    // make a new book object with properties
+    const createdBook = {
+      id: String(Date.now()), // Generate a unique ID based on the current timestamp
+      title: bookInfo.title,
+      author: bookInfo.author,
+      available: true, // New books are available by default
+      createdAt: new Date().toISOString(), // Store the creation date in ISO format
+      updatedAt: new Date().toISOString(), // Store the last updated date in ISO format
+    };
+    // Add the new book to the books array and write it back to the JSON file
+    books.push(createdBook);
+    await writeBookInfo(books);
+    return createdBook;
+  }
+
+  // task two read and display books.
+  async getAllBooks() {
+    // read the book details from books.json using fileHandler.js
+    const books = await readBookInfo();
+    return books;
+  }
+
+  async getOneBook(id) {
+    // read the book details from books.json using fileHandler.js
+    const books = await readBookInfo();
+    const currentBook = books.find((book) => book.id === id);
+    return currentBook;
+  }
+}
