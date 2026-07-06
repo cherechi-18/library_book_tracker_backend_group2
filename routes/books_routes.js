28fd01f8f8125d8e33b28b7e045c3fbda6a5de03
voidmimi <biyibioluwatosin@gmail.com>
2026-07-06 17:04:23 +0100

    added the borrow and return feature.

--- routes/books_routes.js
+++ routes/books_routes.js
@@ -2,22 +2,57 @@
 // Example: GET /books, POST /books, PUT /books/:id, DELETE /books/:id
 // Calls service functions to handle logic
 
-import {Router} from "express";
+import { Router } from "express";
 import { BookService } from "../services/books_services.js";
 const bookRouter = Router();
 const bookService = new BookService();
 
 bookRouter.post("/resources", async (req, res) => {
-    try{
-        const newBook = await bookService.addBook(req.body);
-        res.status(201)
-        res.json({
-            message: "Book added successfully",
-            book: newBook
-        });
-    } catch (error) {
-        res.status(500)
-        res.json({ message: "Error adding book", error: error.message });
-    }
+  try {
+    const newBook = await bookService.addBook(req.body);
+    res.status(201);
+    res.json({
+      message: "Book added successfully",
+      book: newBook,
     });
-    export default bookRouter;
+  } catch (error) {
+    res.status(500);
+    res.json({ message: "Error adding book", error: error.message });
+  }
+});
+
+bookRouter.patch("/borrow/:id", async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const isAvailable = await bookService.borrowBook(req.params.id);
+    console.log(isAvailable);
+    if (isAvailable === false) {
+      return res
+        .status(200)
+        .json({ message: "Request rejected, Book not available Currently" });
+    }
+    res.status(200).json({ message: "Book borrowed successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error borrowing Book", error: error.message });
+  }
+});
+
+bookRouter.patch("/return/:id", async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const isReturned = await bookService.returnBook(req.params.id);
+    if (isReturned === false) {
+      return res
+        .status(200)
+        .json({ message: "Request rejected, Book not Borrowed" });
+    }
+    res.status(200).json({ message: "Book Returned successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error Returning Book", error: error.message });
+  }
+});
+export default bookRouter;
--- services/books_services.js
+++ services/books_services.js
@@ -7,18 +7,19 @@ export class BookService {
   async addBook(bookInfo) {
     // read  boookinfo from books.json through fileHandler.js
     const books = await readBookInfo();
-    // get the last book to get its id
-    let lastId;
+    // get the Id of the last book added
+    let lastId; // variable to store the id of the last book added
     if (books.length === 0) {
+      // if array is empty, assign lastId as zero.
       lastId = 0;
     } else {
-      lastId = books.at(-1).id;
+      lastId = books.at(-1).id; // means check last item in the array(books) and get the id
     }
     //
     // make a new book object with properties
 
     const createdBook = {
-      id: lastId + 1, // Generate a unique ID based on the current timestamp
+      id: lastId + 1, // Generate add one to the lastId. This avoids any error that comes with Deleting item.
       title: bookInfo.title,
       author: bookInfo.author,
       available: true, // New books are available by default
@@ -31,7 +32,53 @@ export class BookService {
     return createdBook;
   }
 
+  // Task 5  Borrowing and Returning books
   async borrowBook(id) {
+    let status = false;
     const books = await readBookInfo();
+    // find book by id.
+    const currentBook = books.find((book) => book.id == id);
+
+    // to prevent errors due to null or undefined value
+    if (!currentBook) {
+      throw Error("Invalid Book ID");
+    }
+
+    // if book is already borrowed, return false.
+    if (currentBook.available === false) {
+      return status;
+    }
+
+    // change the available property to false to prevent double borrowing
+    currentBook.available = false;
+
+    // write the array(books) back to the books.json file.
+    await writeBookInfo(books);
+    status = true;
+    return status;
+  }
+
+  async returnBook(id) {
+    console.log("inside returnBook");
+    console.log(id);
+    let status = false;
+    const books = await readBookInfo();
+    const currentBook = books.find((book) => book.id == id);
+    if (!currentBook) {
+      throw Error("Invalid Book ID");
+    }
+
+    // if book is not borrowed, return false
+    if (currentBook.available === true) {
+      return status;
+    }
+
+    // if book is borrowed change availability back to true. i.e return it!!
+    currentBook.available = true;
+
+    // write array(books) back to books.json
+    await writeBookInfo(books);
+    status = true;
+    return status;
   }
 }
