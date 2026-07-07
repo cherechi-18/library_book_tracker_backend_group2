ec1ef9df79bf489393071e6546cdabce6b8455ce
voidmimi <biyibioluwatosin@gmail.com>
2026-07-07 18:25:01 +0100

    validation for create/add book

--- services/books_services.js
+++ services/books_services.js
@@ -3,29 +3,24 @@
 // Uses fileHandler (read/write JSON) to update books.json
 
 import { readBookInfo, writeBookInfo } from "../utils/fileHandler.js";
+import { checkBookExist } from "../utils/validation.js";
 export class BookService {
   async addBook(bookInfo) {
+    // to check if book already exists
+    await checkBookExist(bookInfo);
+
     // read  boookinfo from books.json through fileHandler.js
     const books = await readBookInfo();
-    // get the Id of the last book added
-    let lastId; // variable to store the id of the last book added
-    if (books.length === 0) {
-      // if array is empty, assign lastId as zero.
-      lastId = 0;
-    } else {
-      lastId = books.at(-1).id; // means check last item in the array(books) and get the id
-    }
-    //
     // make a new book object with properties
-
     const createdBook = {
-      id: String(Date.now()), // Generate a unique ID based on the current timestamp
+      id: Date.now(), // Generate a unique ID based on the current timestamp
       title: bookInfo.title,
       author: bookInfo.author,
       available: true, // New books are available by default
       createdAt: new Date().toISOString(), // Store the creation date in ISO format
       updatedAt: new Date().toISOString(), // Store the last updated date in ISO format
     };
+
     // Add the new book to the books array and write it back to the JSON file
     books.push(createdBook);
     await writeBookInfo(books);
@@ -59,8 +54,6 @@ export class BookService {
   }
 
   async returnBook(id) {
-    console.log("inside returnBook");
-    console.log(id);
     let status = false;
     const books = await readBookInfo();
     const currentBook = books.find((book) => book.id == id);
--- utils/validation.js
+++ utils/validation.js
@@ -1,3 +1,63 @@
 // Validation rules for book data
 // Example: check required fields (title, author), generate IDs, add timestamps
 // Prevents invalid or incomplete data from being saved
+
+import { readBookInfo, writeBookInfo } from "../utils/fileHandler.js";
+
+export const validateAddBookReq = (req, res, next) => {
+  // destructure the req.body
+  const { title, author } = req.body;
+
+  if (!title || !author) {
+    return res
+      .status(400)
+      .json({ error: "Missing required field title or author" });
+  }
+
+  if (typeof title !== "string" || typeof author !== "string") {
+    return res.status(400).json({ error: "Invalid field title or author" });
+  }
+
+  next();
+};
+
+export async function checkBookExist(bookInfo) {
+  const { title, author } = bookInfo;
+
+  const books = await readBookInfo();
+  if (books.length === 0) {
+    return;
+  }
+  const exist = books.find(
+    (book) =>
+      book.title.toLowerCase() === title.toLowerCase() &&
+      book.author.toLowerCase() === author.toLowerCase(),
+  );
+  if (exist) {
+    throw Error("Book already Exists");
+  }
+
+  return;
+}
+
+export async function confirmNewBook(bookInfo) {
+  const { id, title, author, available, createdAt, updatedAt } = bookInfo;
+  const books = await readBookInfo();
+
+  if (!id || !title || !author || !available || !createdAt || !updatedAt) {
+    throw Error("Incomplete data, cannot save");
+  }
+
+  if (
+    typeof id !== "number" ||
+    typeof title !== "string" ||
+    typeof author !== "string" ||
+    typeof available !== "boolean" ||
+    typeof createdAt !== "string" ||
+    typeof updatedAt !== "string"
+  ) {
+    throw Error("Incompatable datatype, cannot save");
+  }
+
+  return;
+}
