0da712f590fab53f2ba76bae4d4a5cd40abd8e6f
voidmimi <biyibioluwatosin@gmail.com>
2026-07-07 14:26:57 +0100

    Update books_services.js
    Changed the id to Date.now()

--- services/books_services.js
+++ services/books_services.js
@@ -3,25 +3,24 @@
 // Uses fileHandler (read/write JSON) to update books.json
 
 import { readBookInfo, writeBookInfo } from "../utils/fileHandler.js";
-export class BookService {
-  async addBook(bookInfo) {
-    // read  boookinfo from books.json through fileHandler.js
-    const books = await readBookInfo();
-    // make a new book object with properties
-
+ async addBook(bookInfo) {
+        // read  boookinfo from books.json through fileHandler.js
+   const books = await readBookInfo();
+        // make a new book object with properties     
     const createdBook = {
-      id: String(books.length + 1), // auto updating from 1
-      title: bookInfo.title,
-      author: bookInfo.author,
-      available: true, // New books are available by default
-      createdAt: new Date().toISOString(), // Store the creation date in ISO format
-      updatedAt: new Date().toISOString(), // Store the last updated date in ISO format
+     id: String(Date.now()), // Generate a unique ID based on the current timestamp
+     title: bookInfo.title,
+     author: bookInfo.author,
+     available: true, // New books are available by default
+     createdAt: new Date().toISOString(), // Store the creation date in ISO format
+     updatedAt: new Date().toISOString() // Store the last updated date in ISO format
     };
-    // Add the new book to the books array and write it back to the JSON file
-    books.push(createdBook);
-    await writeBookInfo(books);
-    return createdBook;
-  }
+// Add the new book to the books array and write it back to the JSON file
+        books.push(createdBook);
+        await writeBookInfo(books);
+        return createdBook;
+
+}
 
   // task two read and display books.
   async getAllBooks() {
